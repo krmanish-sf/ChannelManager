5a9e78f79c0671d35f3e0e6a7ac196f11b28cd9e
Amit Yadav <yadav.amitsingh@gmail.com>
2015-07-20 15:11:11 +0530

    Added CORS Filter and Changes to Order Audit Trail

---
+++ cm-rest/src/main/java/com/is/cm/rest/controller/SimpleCORSFilter.java
@@ -0,0 +1,48 @@
+package com.is.cm.rest.controller;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleCORSFilter implements Filter {
+
+	private static final Logger log = LoggerFactory
+			.getLogger(SimpleCORSFilter.class);
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain chain) throws IOException, ServletException {
+		log.info("Adding CORS Header");
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "http://staging.inventorysource.com");
+		response.setHeader("Access-Control-Allow-Methods",
+				"POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "content-type");
+		chain.doFilter(req, res);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) {
+		log.info("Initializing CORS filter.");
+		if (filterConfig == null) {
+			throw new RuntimeException("Error oin filter");
+		}
+
+	}
+
+	public void destroy() {
+	}
+
+}
