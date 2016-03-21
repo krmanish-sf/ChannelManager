9224340337c3c9fbbe769b1ae6f1c5ffb679233a
Manish <manish@inventorysource.com>
2016-03-21 18:29:04 +0530

    ISD-382 : Dev Hub Platform integration

---
+++ oim-core/src/main/java/salesmachine/util/DevhubShippingMapping.java
@@ -0,0 +1,42 @@
+package salesmachine.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DevhubShippingMapping {
+
+  private static final Logger log = LoggerFactory
+      .getLogger(DevhubShippingMapping.class);
+  private static final Properties prop = new Properties();
+  private static InputStream input = null;
+  static {
+    try {
+      String filename = "dev-hub-shipping.properties";
+      input = ApplicationProperties.class.getClassLoader()
+          .getResourceAsStream(filename);
+      prop.load(input);
+    } catch (IOException ex) {
+      log.error(ex.getMessage(), ex);
+    } finally {
+      if (input != null) {
+        try {
+          input.close();
+        } catch (IOException e) {
+          log.error(e.getMessage(), e);
+        }
+      }
+    }
+  }
+
+  private DevhubShippingMapping() {
+  }
+
+  public static final String getProperty(String key) {
+    key = key.trim().replaceAll(" ", "_");
+    return prop.getProperty(key);
+  }
+}
